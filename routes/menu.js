1105672a68141349601e0d69d48ef4b9562f0b4e
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-26 19:46:57 +0000

    add DB queries

--- routes/menu.js
+++ routes/menu.js
@@ -5,10 +5,7 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-
-module.exports = (db) => {
+module.exports = (router, db) => {
   router.get("/", (req, res) => {
     //get menu display for main page no taking order
     res.send("Menu");
