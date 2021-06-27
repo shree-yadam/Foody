bddd139397a437c626c3dff367277a7e12b86c85
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-27 01:19:34 +0000

    remove app.js script from index.ejs

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,7 +1,7 @@
 /*
  * All routes for Restaurants are defined here
  * Since this file is loaded in server.js into api/restaurants,
- *   these routes are mounted onto /users
+ *   these routes are mounted onto /api/restaurants
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
