9c73944837f1b1dcdc0a48d28be33ca085c23484
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-28 16:49:33 +0000

    sms starting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
 const cookieSession = require('cookie-session');
-
+const sms = require('./lib/sms/sendSMS');
 const router = express.Router();
 
 //TBD :HARD CODED RESTAURANT ID TO BEGIN
@@ -90,6 +90,9 @@ app.get("/", (req, res) => {
     res.send(e);
   });
 });
+
+sms.sendSMS()
+.then(message => console.log(message.sid));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
